b0250de2fab1af5011f58db9e6ce29933997329d
evgeniyChepelev <68751844+evgeniyChepelev@users.noreply.github.com>
2026-07-24 10:47:28 +0200

    gdsgd

--- PawMate/PawMateApp.swift
+++ PawMate/PawMateApp.swift
@@ -2,11 +2,11 @@ import SwiftUI
 
 @main
 struct PawMateApp: App {
-    @UIApplicationDelegateAdaptor(PushCoordinator.self) var pushCoordinator
+//    @UIApplicationDelegateAdaptor(PushCoordinator.self) var pushCoordinator
 
     var body: some Scene {
         WindowGroup {
-            RootRouterView()
+            Color.red
         }
     }
 }
